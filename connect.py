18d146d71d1d53b0920d54cd9e0d8938ed28587f
MutaharS <mutahar014@gmail.com>
2021-07-15 18:55:55 -0700

    connect.py

--- connect.py
+++ connect.py
@@ -1,27 +1,51 @@
 import psycopg2
 
+# If admin, then connect like this
 conn = psycopg2.connect(
     host="localhost", 
     port = 5432, 
     database="bookstore", 
-    user="postgres", 
+    user="librarian", 
     password="finalproject"
 )
 
-# Regular User FUNCTIONALITY (Need to create a user in pgadmin4
-# and grant them proper priviliges)
+# Default is
+conn = psycopg2.connect(
+    host="localhost", 
+    port = 5432, 
+    database="bookstore", 
+    user="patron", 
+    password="finalproject"
+)
+
+# Regular User FUNCTIONALITY
 # signup view
 # login view
 # search view
     # Do you want to search by subject
+        # book results view 
     # Do you want to search by author
+        # book results view
+    # Do you want to see your checked out books (include their due dates)
+
+    # Extra Feature (choose subject): WHERE subject = '' ORDER BY RANDOM LIMIT 1
     # Do you want to be recommended a random book?
-# book results view 
 
+# Librarian 
+    # Assign book to a user:
+        # Input: Email
+        # Input: ISBN
+        # Create the borrow entry (INSERT)
 
+    # Return book from user:
+        # Input: Email
+        # Input: ISBN
+        # Delete the borrow entry (INSERT)
 
-# Create a cursor object
-cur = conn.cursor()
-cur.execute("SELECT * FROM books WHERE subject = %s ", [subject])
+    # View all emails
+    # View all books
+
+# cur = conn.cursor()
+# cur.execute("SELECT * FROM books WHERE subject = %s ", [subject])
 
 print(cur.fetchone()[0])
